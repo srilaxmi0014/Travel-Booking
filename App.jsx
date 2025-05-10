7992dfd186f5d8be9c5f3495d7322c8a9a515084
srilaxmi0014 <srilakshmiy2005@gmail.com>
2025-05-11 03:38:37 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -6,9 +6,15 @@ import Signup from './pages/Signup';
 import Booking from './pages/Booking';
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
-import BusBooking from'./pages/BusBooking';
-import TrainBooking from'./pages/TrainBooking';
-import FlightBooking from'./pages/FlightBooking';
+import MapView from './components/MapView';
+import Payment from './pages/Payment';
+import BusBooking from './pages/BusBooking';
+import TrainBooking from './pages/TrainBooking';
+import FlightBooking from './pages/FlightBooking';
+import BookingChoice from './pages/BookingChoice';
+import AIChat from './components/AIChat'; 
+import Menu from './components/Menu'; 
+import AboutUs from './pages/AboutUs'; // Assuming you have an AboutUs page
 
 
 const App = () => {
@@ -20,10 +26,15 @@ const App = () => {
       <Route path="/booking" element={<Booking />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/map" element={<MapView />} />
+      <Route path="/Payment" element={<Payment />} />
       <Route path="/bus-booking" element={<BusBooking />} />
-      <Route path="/flight-booking" element={<FlightBooking />} />
       <Route path="/train-booking" element={<TrainBooking />} />
-      
+      <Route path="/flight-booking" element={<FlightBooking />} />
+      <Route path="/book-choice" element={<BookingChoice />} />
+      <Route path="/menu" element={<Menu />} />
+      <Route path="/aichat" element={<AIChat />} />
+      <Route path="/about-us" element={<AboutUs />} />
       {/* Add more routes as needed */}
     </Routes>
   );
